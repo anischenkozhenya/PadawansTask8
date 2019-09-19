3e1ea42ae48da0f175f5c6e51971962eac672b08
anischenkozhenya <43790223+anischenkozhenya@users.noreply.github.com>
2019-09-19 23:01:57 +0300

    +answer

--- WordsManipulation.cs
+++ WordsManipulation.cs
@@ -1,140 +1,26 @@
 ﻿using System.Text;
 using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace PadawansTask8
 {
     public static class WordsManipulation
     {
         public static void RemoveDuplicateWords(ref string text)
-        {
-            if (text == null)
-                throw new ArgumentNullException();
-            if (text == "")
-                throw new ArgumentException();
-            char[] textchar = text.ToCharArray();
-            //массив слов string 
-            string[] word = new string[0];
-            // массив индексов начала слов word в массиве array
-            int[] intword = new int[0];
-            int FirstLetterOfWord = 0;
-            int lastLetterOfWord = 0;
-            int indexWord = 0;
-            //Медод сохраненияя слова и индекса его певого символа в массиве
-            void Saveword(int first, int last, char[] arraychar, string[] Word)
+        {   
+            string[] words = text.Split(new char[] { '.', ',', '!', '?', '-', ':', ';', ' ' }).Distinct().ToArray();
+            int index;
+            foreach (var item in words)
             {
-                char[] temp = new char[last - first];
-                for (int i = 0; i < (last - first); i++)
-                {
-                    temp[i] = arraychar[i + first];
-                }
-                Word[indexWord] = new string(temp);
-                intword[indexWord] = first;
-                indexWord++;
+                index=text.IndexOf(item)+item.Length;
+                string beforeSeparator;
+                string afterSeparator;
+                beforeSeparator=text.Substring(0,index);
+                afterSeparator=text.Substring(index,text.Length-index);
+                text = text.Substring(0, index) + text.Substring(index, text.Length - index).Replace(item, "");
             }
-            //удаление слов и смещение cимволов 
-            void delete(int longword, int index)
-            {
-                for (int i = intword[index]; i < textchar.Length - longword; i++)
-                {
-                    if (intword[index] + longword + 1 != textchar.Length - 1)
-                    {
-                        textchar[i] = textchar[i + longword];
-                    }
-                }
-                Array.Resize(ref textchar, textchar.Length - longword);
-                for (int i = index; i <= word.Length - 1; i++)
-                {
-                    if (i + 1 <= (word.Length - 1))
-                    {
-                        word[i] = word[i + 1];
-                        intword[i] = intword[i + 1] - longword;
-                    }
-                }
-                Array.Resize(ref word, word.Length - 1);
-                Array.Resize(ref intword, intword.Length - 1);
-            } 
-            //Сравнивание int символа с числом являющимся кодом числа в Юникоде 
-            for (int i = 0; i < textchar.Length; i++)
-            {
-                if (i == 0 && ((65 <= (int)textchar[i] && (int)textchar[i] <= 90)
-                    || (97 <= (int)textchar[i] && (int)textchar[i] <= 122)))
-                //char.IsLetter(textchar[i]))
-                {
-                    FirstLetterOfWord = i;
-                }
-                if (i != 0)
-                {
-
-                    if// (char.IsLetter(textchar[i])&&!char.IsLetter(textchar[i-1]))
-                    (((65 <= (int)textchar[i] && (int)textchar[i] <= 90)
-                    || (97 <= (int)textchar[i] && (int)textchar[i] <= 122))
-                    && (((int)(textchar[i - 1]) == 59)
-                    || ((int)(textchar[i - 1]) == 46)
-                    || ((int)(textchar[i - 1]) == 44)
-                    || ((int)(textchar[i - 1]) == 32)
-                    || ((int)(textchar[i - 1]) == 45)
-                    || ((int)(textchar[i - 1]) == 33)
-                    || ((int)(textchar[i - 1]) == 63)))
-                    {
-                        FirstLetterOfWord = i;
-                    }
-                    if ((i == (textchar.Length - 1))
-                        && ((65 <= (int)textchar[i] && (int)textchar[i] <= 90)
-                    || (97 <= (int)textchar[i] && (int)textchar[i] <= 122)))
-                    //&&char.IsLetter(textchar[i]))
-                    {
-                        lastLetterOfWord = i + 1;
-                        Array.Resize(ref intword, intword.Length + 1);
-                        Array.Resize(ref word, word.Length + 1);
-                        Saveword(FirstLetterOfWord, lastLetterOfWord, textchar, word);
-                    }
-                    if ((((int)(textchar[i]) == 59)
-                    || ((int)(textchar[i]) == 46)
-                    || ((int)(textchar[i]) == 44)
-                    || ((int)(textchar[i]) == 32)
-                    || ((int)(textchar[i]) == 45)
-                    || ((int)(textchar[i]) == 33)
-                    || ((int)(textchar[i]) == 63))
-
-                    &&
-                    ((65 <= (int)textchar[i - 1]
-                    && (int)textchar[i - 1] <= 90)
-                    || (97 <= (int)textchar[i - 1]
-                    && (int)textchar[i - 1] <= 122)))
-                    //!char.IsLetter(textchar[i])&&char.IsLetter(textchar[i-1]))
-                    {
-                        lastLetterOfWord = i;
-                        Array.Resize(ref intword, intword.Length + 1);
-                        Array.Resize(ref word, word.Length + 1);
-                        Saveword(FirstLetterOfWord, lastLetterOfWord, textchar, word);
-                    }
-                }
-
-            }
-            bool ch = true;
-            do
-            {
-                ch = true;
-                for (int i = 0; i < word.Length; i++)
-                {
-                    for (int j = word.Length - 1; j >= 1; j--)
-                    {
-                        if (string.Equals(word[i], word[j]) && i != j)
-                        {
-
-                            delete(word[j].Length, j);
-                            ch = false;
-                            break;
-                        }
-                    }
-                    if (ch == false)
-                    {
-                        break;
-                    }
-                }
-            } while (ch == false);
-            text = new string(textchar);
-
         }
     }
 }
